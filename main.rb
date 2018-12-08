27d8d9426b9b75aab629cb5b96bc7ddeceaba37c
toganaru <drums.apple1015@gmail.com>
2018-12-08 07:35:38 +0000

    mainちょっと修正しました

--- main.rb
+++ main.rb
@@ -6,6 +6,8 @@ Window.load_resources do
   Window.width  = 800
   Window.height = 600
   background_img = Image[:background]
+  background_img.set_color_key([0, 0, 0])
+  
   Window.loop do
   end
 end
