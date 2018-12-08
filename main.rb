c17db1e17bc3f3a76fa99703de75ff9a0c26b381
toganaru <drums.apple1015@gmail.com>
2018-12-08 08:36:25 +0000

    ukishimaの追加

--- main.rb
+++ main.rb
@@ -2,11 +2,14 @@ require 'dxopal'
 include DXOpal
 
 Image.register(:background, 'images/asa.png')
+Image.register(:ukishima,'images/ukishima.png')
 Window.load_resources do
   Window.width  = 800
   Window.height = 600
   background_img = Image[:background]
   background_img.set_color_key([0, 0, 0])
+  ukishima_img = Image[:ukishima]
+  ukishima_img.set_color_key([0, 0, 0])
   
   Window.loop do
   end
