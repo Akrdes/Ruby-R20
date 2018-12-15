caac051df2311292a840ff2f7906910fc876bace
toganaru <drums.apple1015@gmail.com>
2018-12-15 02:03:24 +0000

    画像の表示

--- main.rb
+++ main.rb
@@ -1,6 +1,8 @@
 require 'dxopal'
 include DXOpal
 
+#require_remote 'map.rb'
+
 Image.register(:background, 'images/asa.png')
 Image.register(:ukishima,'images/ukishima.png')
 Window.load_resources do
@@ -9,8 +11,11 @@ Window.load_resources do
   background_img = Image[:background]
   background_img.set_color_key([0, 0, 0])
   ukishima_img = Image[:ukishima]
-  ukishima_img.set_color_key([0, 0, 0])
-  
+  ukishima_img.set_color_key([255, 255, 255])
+
   Window.loop do
+    Window.draw(0,-1200,background_img)
+    Window.draw(500,200,ukishima_img)
+    
   end
 end
--- map.rb
+++ map.rb
@@ -1,3 +1,4 @@
 # coding: utf-8
 
-class Background < Sprite
+class Map < Sprite
+  
