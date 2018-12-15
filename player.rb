c414c75396b089789fcbd49402c295ea8987fd97
Hosokawa-Hiroto <hososhi0820@gmail.com>
2018-12-15 04:42:10 +0000

    ジャンプ+横移動

--- player.rb
+++ player.rb
@@ -1,23 +1,53 @@
 # coding: utf-8
 
 class Player < Sprite
-  attr_accessor :v,:bottom
+  attr_accessor :x1,:y1,:move,:jflag,:vy,:vy0,:g
 
   def initialize(x,y,image)
-    @v = 0
-    @bottom = y + 32 # 32x32の画像の場合
+    @x1 = x * 2
+    @y1 = y
+    @move = 4.0
+    @jflag = false
+    @vy = 0
+    @vy0 = -12.0
+    @g = 0.5
     super
   end
   
-  def jump
-    @v = -20 if(Input.key_push?(K_SPACE))
-    @bottom += @v
-    if(@bottom >= 321)
-      @bottom = 320
-      @v = 0
-    end
-    self.y = @bottom - 32
-    @v += 1 # 重力加速度
+  def update
+	if (Input.key_down?(K_RIGHT) && self.x < @x1-self.image.width ) # → キー を押したか
+		self.x += @move;
+	end
+	if (Input.key_down?(K_LEFT) && self.x > 0 ) # ← キー を押したか
+		self.x -= @move;
+	end
+	
+	if (@jflag)
+		self.y += @vy;
+		if ( @vy == @vy0 * (-1.0) )
+			@jflag = false;
+		end
+		@vy += @g;
+	else 
+		if (Input.key_down?(K_SPACE))
+			@jflag = true;
+			@vy = @vy0;
+		end
+	end
+	
+#	if(@jflag == true)
+#		@y_temp = self.y;
+#		self.y += (self.y - @y_prev) + 1;
+#		@y_prev = @y_temp;
+#		if(self.y == @y1)
+#			@jflag = false;
+#		end
+#	end
+#	
+#	if(Input.key_down?(K_SPACE) && @jflag == false)
+#		@jflag = true;
+#		@y_prev = self.y;
+#		self.y = self.y - 20;
+#	end
   end
-  
 end
