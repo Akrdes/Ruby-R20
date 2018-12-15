a48b055a9a4e2355426005222e7a9fc8f604460f
toganaru <drums.apple1015@gmail.com>
2018-12-15 05:31:13 +0000

    浮島複数表示化

--- main.rb
+++ main.rb
@@ -1,21 +1,23 @@
 require 'dxopal'
 include DXOpal
 
-#require_remote 'map.rb'
+require_remote 'map.rb'
 
 Image.register(:background, 'images/asa.png')
 Image.register(:ukishima,'images/ukishima.png')
 Window.load_resources do
-  Window.width  = 800
-  Window.height = 600
+  Window.width  = 600
+  Window.height = 800
   background_img = Image[:background]
   background_img.set_color_key([0, 0, 0])
   ukishima_img = Image[:ukishima]
   ukishima_img.set_color_key([255, 255, 255])
 
+  map = Map.new()
+  
   Window.loop do
-    Window.draw(0,-1200,background_img)
-    Window.draw(500,200,ukishima_img)
-    
+    Window.draw(0,-1000,background_img)
+    #Window.draw(300,200,ukishima_img)
+    map.draw
   end
 end
--- map.rb
+++ map.rb
@@ -1,4 +1,22 @@
 # coding: utf-8
 
 class Map < Sprite
-  
+  def draw
+    uki = []
+    uki<<Uki.new(50,500)      
+    uki<<Uki.new(10,90)
+    
+    for i in uki  
+        Window.draw(i.x,i.y,Image[:ukishima])
+    end
+       
+  end
+end
+
+class Uki
+  attr_accessor :x,:y
+  def initialize(x,y)
+    @x = x
+    @y = y
+  end
+end
