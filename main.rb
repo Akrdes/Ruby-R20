5fabdc0f0e491e6130cb9c013521d9f31c19b23e
Akrdes <akrdsy@outlook.jp>
2018-12-21 00:07:44 +0000

    main.rb岩田論

--- main.rb
+++ main.rb
@@ -13,15 +13,14 @@ Image.register(:title_msg,'images/title_msg.png')
 Image.register(:clear,'images/clear.png')
 Image.register(:over,'images/over.png')
 
+Image.register(:return,'images/returnt.png')
+Image.register(:retry,'images/retry.png')
+Image.register(:ohh,'images/ohh.jpg')
+
+
 Window.load_resources do
   Window.width  = 600
   Window.height = 800
-
-#  background_img = Image[:background]
-#  background_img.set_color_key([0, 0, 0])
-#  ukishima_img = Image[:ukishima]
-#  ukishima_img.set_color_key([255, 255, 255])
-
 
   game=Game.new
   game.run
