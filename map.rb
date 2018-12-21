7d3a221ad942e1814bf8f2cf1ebffb8e38863122
Hosokawa-Hiroto <hososhi0820@gmail.com>
2018-12-21 09:22:59 +0000

    キャラクターが浮島に乗る，落ちはしない

--- map.rb
+++ map.rb
@@ -1,4 +1,19 @@
 # coding: utf-8
 
 class Map < Sprite
-  
+
+end
+
+class Uki < Sprite
+  def hit
+    shift_y = 761 - self.y
+    $ukis.each do |uki|
+      uki.y += shift_y
+    end
+    $map.y += shift_y
+    $count += 1
+    if $count >10
+        p 123
+    end
+  end
+end
---
+++ player.rb
@@ -0,0 +1,45 @@
+# coding: utf-8
+
+class Player < Sprite
+  attr_accessor :x1,:y1,:move,:jflag,:vy,:vy0,:g,:pl
+
+  def initialize(x,y,image)
+    @x1 = x * 2
+    @y1 = y
+    @move = 4.0
+    @jflag = false
+    @vy = 0
+    @vy0 = -13.0
+    @g = 1.0
+    super
+  end
+  
+  def update(island)
+	if (Input.key_down?(K_RIGHT) && self.x < @x1-self.image.width ) # →キーを押したか
+		self.x += @move;
+	end
+	if (Input.key_down?(K_LEFT) && self.x > 0 ) #←キーを押したか
+		self.x -= @move;
+	end
+	
+	if (@jflag)
+		self.y += @vy;
+		self.collision=[self.x, self.y, self.x+self.image.width, self.y+self.image.height]
+		Sprite.check(self, island)
+		if ( @vy == @vy0 * (-1.0) )
+			@jflag = false;
+		end
+		@vy += @g;
+	else 
+		if (Input.key_down?(K_SPACE))
+			@jflag = true;
+			@vy = @vy0;
+		end
+	end
+  end
+  
+  def shot
+    @jflag = false;
+    self.y = $ukis[$count-1].y - (self.image.height)
+  end  
+end
